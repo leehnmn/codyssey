9fa6ddc32e5e227631deff8ace9137962f24780a
이현민 <dlgusals0120@c4r1s7.codyssey.kr>
2025-08-12 17:40:34 +0900

    충돌실험

--- main -1.2 -2.1/app.py
+++ main -1.2 -2.1/app.py
@@ -7,6 +7,9 @@ def home():
 @app.route("/menu")
 def menu():
     return render_template("menu.html")
+@app.route("/test1")
+def test1():
+    return render_template('test1.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
