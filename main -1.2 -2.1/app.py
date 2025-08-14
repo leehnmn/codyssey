5e2e16e8f1057adc0791cb804f969a2cd32b7c43
leehnmn <dlgusals0120@gmail.com>
2025-08-14 13:48:44 +0900

    Update app.py

--- main -1.2 -2.1/app.py
+++ main -1.2 -2.1/app.py
@@ -10,9 +10,13 @@ def home():
 def menu():
     return render_template("menu.html")
 
+@app.route("/test1")
+def test1():
+    return render_template('test1.html')
+    
 @app.route("/test2")
 def test2():
     return render_template('test2.html')
-
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
